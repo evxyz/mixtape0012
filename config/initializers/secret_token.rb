31d3f15172f2f281d3f5bebd69637f00cda78081
evx001 <eviexeris@yahoo.com>
2012-05-04 00:44:35 -0400

    Initial+song+artist_tables

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mixtape0012::Application.config.secret_token = 'f822079019b10c828b8de2823af1fcd41bb885481c203e23c15468b66627b2737033120e0623653798d4250eb735283f2ad3809f17196e2b24c836e1292975ff'
